fb2fc34bcaf6ebd8e1b1c71e33461b28307c8d50
unknown <liceum23@S7-LICEUM-24.pcoip.ki.agh.edu.pl>
2017-02-12 14:58:31 +0100

    Zmiany dokonane na zajeciach

--- src/main/java/pl/edu/agh/mwo/invoice/Invoice.java
+++ src/main/java/pl/edu/agh/mwo/invoice/Invoice.java
@@ -1,27 +1,52 @@
 package pl.edu.agh.mwo.invoice;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
 
 import pl.edu.agh.mwo.invoice.product.Product;
 
 public class Invoice {
+	private ArrayList<Product> products = new ArrayList<Product>();
+	
 	public void addProduct(Product product) {
-		// TODO: implement
+		products.add(product);
 	}
 
 	public void addProduct(Product product, Integer quantity) {
-		// TODO: implement
+		if(quantity == null || quantity <= 0) {
+			throw new IllegalArgumentException("Niepoprawna ilosc");
+		}
+		
+		for(int i = 0; i < quantity; i++) {
+			this.addProduct(product);
+		}
 	}
 
 	public BigDecimal getSubtotal() {
-		return null;
+		BigDecimal subtotal = new BigDecimal("0");
+		for (Product product : products){
+			subtotal = subtotal.add(product.getPrice());
+		}
+		return subtotal;
 	}
 
 	public BigDecimal getTax() {
-		return null;
+		BigDecimal tax = new BigDecimal("0");
+		for(Product product : this.products) {
+			tax = tax.add(product.getPrice().multiply(product.getTaxPercent()));
+		}
+		
+		return tax;
 	}
 
 	public BigDecimal getTotal() {
-		return null;
+		BigDecimal total = new BigDecimal("0");
+		for(Product product : this.products) {
+			total = total.add(product.getPriceWithTax());
+		}
+		
+		total = total.setScale(2, BigDecimal.ROUND_DOWN);
+		return total;
 	}
 }
+
--- src/main/java/pl/edu/agh/mwo/invoice/product/Product.java
+++ src/main/java/pl/edu/agh/mwo/invoice/product/Product.java
@@ -13,21 +13,39 @@ public abstract class Product {
 		this.name = name;
 		this.price = price;
 		this.taxPercent = tax;
+		
+		if (name == null) {
+			  throw new IllegalArgumentException("Nie można stworzyc produktu o nazwie NULL");
+			}
+		if (name.length() == 0) {
+			  throw new IllegalArgumentException("Nie można stworzyc produktu bez nazwy");
+			}
+		if (price == null) {
+			  throw new IllegalArgumentException("Nie można stworzyc produktu o cenie NULL");
+			}
+		if (price.equals(BigDecimal.ZERO)) {
+			  throw new IllegalArgumentException("Nie można stworzyc produktu z zerowa cena");
+			}
+		if (price.compareTo(BigDecimal.ZERO) <0){
+			 throw new IllegalArgumentException("Nie można stworzyc produktu z ujemna cena");
+		}
+
 	}
 
 	public String getName() {
-		return null;
+		return this.name;
 	}
 
 	public BigDecimal getPrice() {
-		return null;
+		return this.price;
 	}
 
 	public BigDecimal getTaxPercent() {
-		return null;
+		return this.taxPercent;
 	}
 
 	public BigDecimal getPriceWithTax() {
-		return null;
+		BigDecimal taxValue = this.price.multiply(this.taxPercent) ;
+		return taxValue.add(this.price);
 	}
 }
